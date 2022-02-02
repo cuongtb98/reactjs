6e0b06c9d067270895491b72c4275fc755770e89
cuong <cuong98nguyen@gmail.com>
2022-02-03 00:51:32 +0700

    input google

---
+++ form_input/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import InputGoogle from './components/InputGoogle/InputGoogle';
+
+function App() {
+  return (
+    <div className="app">
+      <InputGoogle />
+    </div>
+  );
+}
+
+export default App;
---
+++ form_input/src/components/InputGoogle/InputGoogle.js
@@ -0,0 +1,12 @@
+import "./style.css"
+
+const InputGoogle = () => {
+    return (  
+        <div className="App">
+            <input placeholder=" " />
+            <label>Name</label>
+        </div>
+    );
+}
+ 
+export default InputGoogle;
